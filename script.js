6b3dcf926eb44ad091b16e41728dfce5e9eecae9
hilubabz <utsargam44@gmail.com>
2024-01-24 17:05:07 +0545

    aaideko

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+let button = document.querySelector("#openDialog");
+let dialog = document.querySelector("#dialog");
+
+
+button.addEventListener("mouseover" ,function(){
+   dialog.show()
+});
