d3c5976d90f078cbc097ca37c0ba25408a3ef35f
hilubabz <utsargam44@gmail.com>
2024-01-27 17:56:45 +0545

    yes

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-console.log("Hello World");
-let button = document.querySelector("#openDialog");
-let dialog = document.querySelector("#dialog");
+let images = document.querySelectorAll('.photo-slider');
+  let currentIndex = 0;
 
+  function showImage(index) {
+    images.forEach((element, i) => {
+      if (i === index) {   
+        element.classList.add('active');
+      } else {
+        element.classList.remove('active');
+      }
+    });
+  }
 
-button.addEventListener("mouseover" ,function(){
-   dialog.show()
-});
+  function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  images[currentIndex].classList.add('active');
+  let sliderInterval = setInterval(changeImage, 2000);
