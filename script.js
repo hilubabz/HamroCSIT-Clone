d055b45cd0e25c3b5537d581096cb83559ced792
hilubabz <utsargam44@gmail.com>
2024-05-22 22:23:31 +0545

    yes

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 let images = document.querySelectorAll('.photo-slider');
-  let currentIndex = 0;
+let currentIndex = 0;
 
-  function showImage(index) {
-    images.forEach((element, i) => {
-      if (i === index) {   
-        element.classList.add('active');
-      } else {
-        element.classList.remove('active');
-      }
-    });
+function showImage(index) {
+  images.forEach((element, i) => {
+    if (i === index) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
+
+function changeImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+images[currentIndex].classList.add('active');
+let sliderInterval = setInterval(changeImage, 2000);
+
+let button = document.querySelector('.dark_mode')
+
+let signin = document.querySelector('.account-extension');
+let count = 0;
+function dekhyo() {
+  if (count % 2 == 0) {
+    signin.classList.add('dekhyo');
   }
-
-  function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
+  else {
+    signin.classList.remove('dekhyo');
   }
-
-  images[currentIndex].classList.add('active');
-  let sliderInterval = setInterval(changeImage, 2000);
-
-let button=document.querySelector('.dark_mode')
+  count++;
+}
